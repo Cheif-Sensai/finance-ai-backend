c5ca5afb2d8f265b63a442e42871df5c704b50c8
Kuwer <bhendarkarkuwer@gmail.com>
2024-09-04 14:39:09 +0530

    initial nodejs and express setup

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.listen(3000, () => {
+    console.log('Listening on 3000 :'
+                + '\n Na akarha to darde, na aukrha to darde'
+                + '\n Pindo utho bhau aa valat ann parhde'
+                + '\n Shehar aa k, chaki kali GT yaaran ne'
+                + '\n Paise thele da khyal nhio rata rakhiya')
+});
